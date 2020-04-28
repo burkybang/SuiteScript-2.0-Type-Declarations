0fd25a8ccf57ec99719b104f8b1e6527d048f9af
Adam Smith <burkybang@gmail.com>
2020-04-27 22:34:12 -0400

    N/currency: Converted to TypeScript

---
+++ N/currency.d.ts
@@ -0,0 +1,30 @@
+/**
+ * SuiteScript currency module
+ * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4358551775.html
+ *
+ * @module N/currency
+ * @NApiVersion 2.x
+ */
+interface currency {
+  
+  /**
+   * Retrieves the exchange rate between two currencies based on the specified date.
+   * The return value comes from the Exchange Rate column of the Currency Exchange Rates record.
+   * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4358678787.html
+   *
+   * @governance 10 units
+   *
+   * @param {Object} options
+   * @param {number|string} options.source The source currency ID or String
+   * @param {number|string} options.target The target currency ID or String
+   * @param {Date} options.date [optional] The date of the desired effective currency rate. Defaults to today.
+   *
+   * @return {number}
+   *
+   * @throws {SuiteScriptError} SSS_MISSING_REQD_ARGUMENT if id parameter is missing
+   * @throws {SuiteScriptError} SSS_INVALID_CURRENCY_ID if an invalid currency (source or target) is specified
+   *
+   * @since 2015.2
+   */
+  exchangeRate(options: { source: number | string, target: number | string, date?: Date }): number
+}
