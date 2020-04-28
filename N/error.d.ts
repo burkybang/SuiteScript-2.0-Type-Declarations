1dc4234a37fd01b8699f126a12d8f9367ce18d52
Adam Smith <burkybang@gmail.com>
2020-04-27 22:42:18 -0400

    N/error: Corrected @see links

--- N/error.d.ts
+++ N/error.d.ts
@@ -1,6 +1,6 @@
 /**
  * SuiteScript error module
- * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4243798608.html
+ * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4243798608.html
  *
  * @module N/error
  * @NApiVersion 2.x
@@ -9,7 +9,7 @@ interface error {
   
   /**
    * Create a new Error object
-   * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4243803203.html
+   * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4243803203.html
    *
    * @param {Object} options
    * @param {string} options.name
@@ -24,7 +24,7 @@ declare namespace error {
   
   /**
    * Encapsulates a custom SuiteScript error for any server script type that is not a user event script
-   * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4253432660.html
+   * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4253432660.html
    *
    * @restriction Server scripts that are not user event scripts and client scripts
    *
@@ -35,7 +35,7 @@ declare namespace error {
     
     /**
      * Error ID that is automatically generated when a new error is created
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4243803497.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4243803497.html
      *
      * @name SuiteScriptError#id
      * @type {string}
@@ -47,7 +47,7 @@ declare namespace error {
     
     /**
      * User-defined error code
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4243803552.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4243803552.html
      *
      * @name SuiteScriptError#name
      * @type {string}
@@ -59,7 +59,7 @@ declare namespace error {
     
     /**
      * Error message text displayed in the Details column of the Execution Log
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4243803629.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4243803629.html
      *
      * @name SuiteScriptError#message
      * @type {string}
@@ -72,7 +72,7 @@ declare namespace error {
     /**
      * List of method calls that the script is executing when the error is thrown
      * The most recently executed method is listed at the top of the list.
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4243803715.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4243803715.html
      *
      * @name SuiteScriptError#stack
      * @type {string[]}
@@ -117,7 +117,7 @@ declare namespace error {
   
   /**
    * Encapsulates a custom SuiteScript error for a user event script
-   * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4253440386.html
+   * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4253440386.html
    *
    * @restriction User event scripts
    *
@@ -128,7 +128,7 @@ declare namespace error {
   
     /**
      * Error ID that is automatically generated when a new error is created
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4254922120.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4254922120.html
      *
      * @name UserEventError#id
      * @type {string}
@@ -140,7 +140,7 @@ declare namespace error {
   
     /**
      * User-defined error code
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4254922635.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4254922635.html
      *
      * @name UserEventError#name
      * @type {string}
@@ -152,7 +152,7 @@ declare namespace error {
   
     /**
      * Error message text displayed in the Details column of the Execution Log
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4254962538.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4254962538.html
      *
      * @name UserEventError#message
      * @type {string}
@@ -165,7 +165,7 @@ declare namespace error {
     /**
      * List of method calls that the script is executing when the error is thrown
      * The most recently executed method is listed at the top of the list.
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4254923191.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4254923191.html
      *
      * @name UserEventError#stack
      * @type {string[]}
@@ -178,7 +178,7 @@ declare namespace error {
     /**
      * Internal ID of the submitted record that triggered the script
      * This property only holds a value when the error is thrown by an afterSubmit user event script.
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4243803775.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4243803775.html
      *
      * @name UserEventError#recordId
      * @type {string}
@@ -190,7 +190,7 @@ declare namespace error {
     
     /**
      * User event type (beforeLoad, beforeSubmit, or afterSubmit)
-     * @see https://5540748.app.netsuite.com/app/help/helpcenter.nl?fid=section_4243803835.html
+     * @see https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4243803835.html
      *
      * @name UserEventError#eventType
      * @type {'beforeLoad'|'beforeSubmit'|'afterSubmit'}
