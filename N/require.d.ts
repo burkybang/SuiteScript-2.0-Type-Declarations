5c2b030c4b7d0c2d151ccfcfb9fc35459d1bad29
Adam Smith <burkybang@gmail.com>
2021-12-17 16:59:15 -0500

    Improve define and require

--- N/require.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string[]} dependencies list of module paths, either to file cabinet scripts or standard modules (via virtual path)
- * @param {Function} callback which executes once all dependencies are available
- *
- * @throws {error.SuiteScriptError} MODULE_DOES_NOT_EXIST if path is invalid
- *
- * @since 2015.2
- */
-declare function require(dependencies, callback)
