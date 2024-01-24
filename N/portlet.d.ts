3692db0fa17cd5d7409e3196078a76d2fa174610
Adam Smith <burkybang@gmail.com>
2024-01-24 15:22:48 -0500

    N/portlet: Remove unnecessary JSDoc, Add Help Center links

--- N/portlet.d.ts
+++ N/portlet.d.ts
@@ -12,9 +12,10 @@
 interface portlet {
 
   /**
-   * Causes a FORM type portlet to immediately refresh.
+   * Causes a FORM type portlet to immediately refresh
+   * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4473511255}
+   * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_4473511255.html}
    *
-   * @return {void}
    * @throws {error.SuiteScriptError} SSS_INVALID_UI_OBJECT_TYPE if portlet is not FORM type
    *
    * @since 2016.1
@@ -22,9 +23,10 @@ interface portlet {
   refresh(): void;
 
   /**
-   * Causes a FORM type portlet to be immediately resized.
+   * Causes a FORM type portlet to be immediately resized
+   * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4473510937}
+   * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_4473510937.html}
    *
-   * @return {void}
    * @throws {error.SuiteScriptError} SSS_INVALID_UI_OBJECT_TYPE if portlet is not FORM type
    *
    * @since 2016.1
