3dad2372987e1cfa5dbed5078de02d890d51919c
Adam Smith <burkybang@gmail.com>
2024-01-24 15:21:01 -0500

    N/plugin: Remove unnecessary JSDoc, Add Help Center links

--- N/plugin.d.ts
+++ N/plugin.d.ts
@@ -9,27 +9,31 @@
 interface plugin {
 
   /**
-   * Returns the script IDs of implementations of the given custom plugin type. If there's no custom plugin type
+   * Returns the script IDs of implementations of the given custom plug-in type. If there's no custom plug-in type
    * with the given script ID available for the executing script, an empty list is returned.
+   * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4558224168}
+   * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_4558224168.html}
    *
-   * @param {Object} options
-   * @param {string} options.type  script ID of the custom plugin type
-   * @param {boolean} [options.includeDefault]  true if default implementation is to be included in the list; default value is true
-   * @return {string[]} list of scriptIDs of the custom plugin implementations
+   * @param options
+   * @param options.type Script ID of the custom plug-in type
+   * @param [options.includeDefault=true] True if default implementation is to be included in the list; default value is true
+   * @return List of scriptIDs of the custom plug-in implementations
    */
   findImplementations(options: {
     type: string,
-    scriptIDs?: boolean,
+    includeDefault?: boolean,
   }): string[];
 
   /**
-   * Instantiates an implementation of the given custom plugin type. If no implementation ID is explicitly given then
+   * Instantiates an implementation of the given custom plug-in type. If no implementation ID is explicitly given then
    * the implementation which is currently selected in the UI (Manage Plug-ins page) will be returned.
+   * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4558229654}
+   * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_4558229654.html}
    *
-   * @param {Object} options
-   * @param {string} options.type  script ID of the custom plugin type
-   * @param {string} [options.implementation]  script ID of the custom plugin implementation
-   * @return {Object} an object implementing the custom plugin type
+   * @param options
+   * @param options.type Script ID of the custom plug-in type
+   * @param [options.implementation] Script ID of the custom plug-in implementation
+   * @return An object implementing the custom plug-in type
    */
   loadImplementation(options: {
     type: string,
