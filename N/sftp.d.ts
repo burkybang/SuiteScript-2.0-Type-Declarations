d0d477b9248d629d11551a55f71d89ffd03844ab
Adam Smith <burkybang@gmail.com>
2024-03-19 16:57:47 -0400

    N/sftp: Remove unnecessary JSDoc

--- N/sftp.d.ts
+++ N/sftp.d.ts
@@ -20,87 +20,72 @@ interface sftp {
    * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1557238823}
    * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_1557238823.html}
    *
-   * @type {1}
-   * @readonly
-   *
    * @throws {error.SuiteScriptError} READ_ONLY_PROPERTY when setting the property is attempted
    *
    * @since 2016.2
    */
-  MIN_CONNECT_TIMEOUT: 1;
+  readonly MIN_CONNECT_TIMEOUT: 1;
 
   /**
    * Constant representing the maximum time allowed for transferring data over connection
    * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1557321286}
    * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_1557321286.html}
    *
-   * @type {20}
-   * @readonly
-   *
    * @throws {error.SuiteScriptError} READ_ONLY_PROPERTY when setting the property is attempted
    *
    * @since 2016.2
    */
-  MAX_CONNECT_TIMEOUT: 20;
+  readonly MAX_CONNECT_TIMEOUT: 20;
 
   /**
    * Constant representing the minimun port number
    * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1557327099}
    * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_1557327099.html}
    *
-   * @type {0}
-   * @readonly
-   *
    * @throws {error.SuiteScriptError} READ_ONLY_PROPERTY when setting the property is attempted
    *
    * @since 2019.2
    */
-  MIN_PORT_NUMBER: 0;
+  readonly MIN_PORT_NUMBER: 0;
 
   /**
    * Constant representing the maximum port number
    * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1557327056}
    * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_1557327056.html}
    *
-   * @type {65535}
-   * @readonly
-   *
    * @throws {error.SuiteScriptError} READ_ONLY_PROPERTY when setting the property is attempted
    *
    * @since 2019.2
    */
-  MAX_PORT_NUMBER: 65535;
+  readonly MAX_PORT_NUMBER: 65535;
 
   /**
    * Constant representing the default port number
    * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1557327135}
    * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_1557327135.html}
    *
-   * @type {22}
-   * @readonly
-   *
    * @throws {error.SuiteScriptError} READ_ONLY_PROPERTY when setting the property is attempted
    *
    * @since 2019.2
    */
-  DEFAULT_PORT_NUMBER: 22;
+  readonly DEFAULT_PORT_NUMBER: 22;
 
   /**
    * Establishes a connection with a remote server and returns a connection object representing that connection
    * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4617005472}
    * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_4617005472.html}
    *
-   * @param {Object} options
-   * @param {string} options.url - host of remote account
-   * @param {string} [options.passwordGuid] - the password guid for remote account
-   * @param {string} options.hostKey - hostKey for ssh server
-   * @param {string} options.username - the username of remote account
-   * @param {number} [options.port=22] - port through which to connect to remote account, defaults to 22
-   * @param {string} [options.directory] - remote directory of connection
-   * @param {number} [options.timeout=Connection.MAX_CONNECT_TIMEOUT] - timeout to establish connection, defaults to Connection.MAX_CONNECT_TIMEOUT
-   * @param {string} [options.hostKeyType] - hostKeyType for ssh server, one of rsa, dsa, ecdsa
-   * @param {string} [options.keyId] - ID of the key to be used for authentication
-   * @return {sftp.Connection} connection - an object that represents the connection
+   * @param options
+   * @param options.url - host of remote account
+   * @param [options.passwordGuid] - the password guid for remote account
+   * @param options.hostKey - hostKey for ssh server
+   * @param options.username - the username of remote account
+   * @param [options.port=22] - port through which to connect to remote account, defaults to 22
+   * @param [options.directory] - remote directory of connection
+   * @param [options.timeout=Connection.MAX_CONNECT_TIMEOUT] - timeout to establish connection, defaults to Connection.MAX_CONNECT_TIMEOUT
+   * @param [options.hostKeyType] - hostKeyType for ssh server, one of rsa, dsa, ecdsa
+   * @param [options.keyId] - ID of the key to be used for authentication
+   * @return connection - an object that represents the connection
    *
    * @throws {error.SuiteScriptError} FTP_UNKNOWN_HOST - thrown when host cannot be found
    * @throws {error.SuiteScriptError} FTP_CONNECT_TIMEOUT_EXCEEDED - thrown when connection takes longer than options.timeout seconds
@@ -136,8 +121,6 @@ declare namespace sftp {
    * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1557239613}
    * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_1557239613.html}
    *
-   * @enum {string}
-   *
    * @since 2019.2
    */
   export enum Sort {
@@ -163,28 +146,22 @@ declare namespace sftp {
      * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1557238937}
      * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_1557238937.html}
      *
-     * @type {300}
-     * @readonly
-     *
      * @throws {error.SuiteScriptError} READ_ONLY_PROPERTY when setting the property is attempted
      *
      * @since 2019.2
      */
-    MAX_TRANSFER_TIMEOUT: 300;
+    readonly MAX_TRANSFER_TIMEOUT: 300;
 
     /**
      * Constant representing the max file size allowed to be transferred
      * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1557238973}
      * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_1557238973.html}
      *
-     * @type {100000000}
-     * @readonly
-     *
      * @throws {error.SuiteScriptError} READ_ONLY_PROPERTY when setting the property is attempted
      *
      * @since 2019.2
      */
-    MAX_FILE_SIZE: 100000000;
+    readonly MAX_FILE_SIZE: 100000000;
 
     /**
      * Downloads a file from the remote server
@@ -193,11 +170,11 @@ declare namespace sftp {
      *
      * @governance 100 points
      *
-     * @param {Object} options
-     * @param {string} options.filename - name of file to download to local machine
-     * @param {string} [options.directory] - relative path to directory of file that will be downloaded. defaults to current directory
-     * @param {number} [options.timeout] - timeout for data transfer, defaults to Connection.MAX_TRANSFER_TIMEOUT (currently 300 seconds)
-     * @return {file.File} file
+     * @param options
+     * @param options.filename - name of file to download to local machine
+     * @param [options.directory] - relative path to directory of file that will be downloaded. defaults to current directory
+     * @param [options.timeout] - timeout for data transfer, defaults to Connection.MAX_TRANSFER_TIMEOUT (currently 300 seconds)
+     * @return file
      *
      * @throws {error.SuiteScriptError} FTP_MAXIMUM_FILE_SIZE_EXCEEDED - thrown if file size is > max file size allowed by NetSuite
      * @throws {error.SuiteScriptError} FTP_INVALID_DIRECTORY - thrown if directory does not exist on server
@@ -221,13 +198,12 @@ declare namespace sftp {
      *
      * @governance 100 points
      *
-     * @param {Object} options
-     * @param {file.File} options.file - file to upload
-     * @param {string} [options.filename] - name to give uploaded file on server, defaults to filename of options.file parameter
-     * @param {string} [options.directory] - relative path to directory where file will be uploaded. defaults to current directory. Illegal characters will be automatically escaped
-     * @param {number} [options.timeout] - timeout for data transfer, defaults to Connection.MAX_TRANSFER_TIMEOUT (very large number TBD)
-     * @param {boolean} [options.replaceExisting] - if true, will replace file on server if a file with options.filename is found at options.directory, if false, will throw an exception if a file with options.filename is found at options.directory, defaults to false
-     * @return {void}
+     * @param options
+     * @param options.file - file to upload
+     * @param [options.filename] - name to give uploaded file on server, defaults to filename of options.file parameter
+     * @param [options.directory] - relative path to directory where file will be uploaded. defaults to current directory. Illegal characters will be automatically escaped
+     * @param [options.timeout] - timeout for data transfer, defaults to Connection.MAX_TRANSFER_TIMEOUT (very large number TBD)
+     * @param [options.replaceExisting] - if true, will replace file on server if a file with options.filename is found at options.directory, if false, will throw an exception if a file with options.filename is found at options.directory, defaults to false
      *
      * @throws {error.SuiteScriptError} FTP_INVALID_DIRECTORY - thrown if directory does not exist on server
      * @throws {error.SuiteScriptError} FTP_TRANSFER_TIMEOUT_EXCEEDED - thrown if transfer takes longer than options.timeout seconds
@@ -252,9 +228,8 @@ declare namespace sftp {
      *
      * @governance 10 points
      *
-     * @param {Object} options
-     * @param {string} options.path - relative path of a directory to be created
-     * @return {void}
+     * @param options
+     * @param options.path - relative path of a directory to be created
      *
      * @throws {error.SuiteScriptError} FTP_PERMISSION_DENIED - thrown when user does not have access to a file or directory on the remote server
      * @throws {error.SuiteScriptError} FTP_DIRECTORY_NOT_FOUND - thrown when path is non-existent directory on the remote server
@@ -272,9 +247,8 @@ declare namespace sftp {
      *
      * @governance 10 points
      *
-     * @param {Object} options
-     * @param {string} options.path - relative path of a directory to be deleted
-     * @return {void}
+     * @param options
+     * @param options.path - relative path of a directory to be deleted
      *
      * @throws {error.SuiteScriptError} FTP_PERMISSION_DENIED - thrown when user does not have access to a file or directory on the remote server
      * @throws {error.SuiteScriptError} FTP_DIRECTORY_NOT_FOUND - thrown when path leads to a non-existent directory on the remote server
@@ -293,9 +267,8 @@ declare namespace sftp {
      *
      * @governance 10 points
      *
-     * @param {Object} options
-     * @param {string} options.path - relative path of a file to be deleted
-     * @return {void}
+     * @param options
+     * @param options.path - relative path of a file to be deleted
      *
      * @throws {error.SuiteScriptError} FTP_PERMISSION_DENIED - thrown when user does not have access to a file or directory on the remote server
      * @throws {error.SuiteScriptError} FTP_NO_SUCH_FILE_OR_DIRECTORY - thrown when path leads to a non-existent file on the remote server
@@ -313,10 +286,9 @@ declare namespace sftp {
      *
      * @governance 10 points
      *
-     * @param {Object} options
-     * @param {string} options.from - relative path of a file to be moved from
-     * @param {string} options.to - relative path of a file to be moved to
-     * @return {void}
+     * @param options
+     * @param options.from - relative path of a file to be moved from
+     * @param options.to - relative path of a file to be moved to
      *
      * @throws {error.SuiteScriptError} FTP_INVALID_MOVE - thrown when source is not readable or the target is not writable or source or target does not exist on the remote server
      *
@@ -331,14 +303,12 @@ declare namespace sftp {
      * Lists the files and directories in a directory on the remote server
      * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_1557235176}
      * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_1557235176.html}
-     * @
      *
      * @governance 10 points
      *
-     * @param {Object} options
-     * @param {string} options.path - relative path of a file that will be downloaded
-     * @param {Sort} [options.sort] - sort option
-     * @return {Object[]}
+     * @param options
+     * @param options.path - relative path of a file that will be downloaded
+     * @param [options.sort] - sort option
      *
      * @throws {error.SuiteScriptError} FTP_INVALID_DIRECTORY - thrown when path leads to a non-existent directory on the remote server
      * @throws {error.SuiteScriptError} FTP_PERMISSION_DENIED - thrown when user does not have access to a file or directory on the remote server
